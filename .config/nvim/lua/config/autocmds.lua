af93e354e97102a1baa720253ddecda62f09f843
Vladimir <18351400+en9inerd@users.noreply.github.com>
2025-01-30 20:41:07 -0500

    update nvim config

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -11,39 +11,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
--- Save current session of project by working directory
--- vim.api.nvim_create_autocmd('VimLeavePre', {
---   desc = 'Save current session of project by working directory',
---   group = vim.api.nvim_create_augroup('save-session', { clear = true }),
---   callback = function()
---     if vim.g.savesession then
---       local cwd = vim.fn.getcwd()
---       local base = vim.fn.fnamemodify(cwd, ':p:h:t')
---       local hash = vim.fn.sha256(cwd):sub(1, 8)
---       local session_file = vim.fn.stdpath 'data' .. '/sessions/' .. base .. '-' .. hash .. '.vim'
---
---       vim.fn.mkdir(vim.fn.stdpath 'data' .. '/sessions', 'p')
---       vim.cmd('mksession! ' .. vim.fn.fnameescape(session_file))
---     end
---   end,
--- })
-
--- Restore session of project by working directory
--- vim.api.nvim_create_autocmd('VimEnter', {
---   desc = 'Restore session of project by working directory',
---   group = vim.api.nvim_create_augroup('restore-session', { clear = true }),
---   callback = function()
---     if vim.g.savesession then
---       local cwd = vim.fn.getcwd()
---       local base = vim.fn.fnamemodify(cwd, ':p:h:t')
---       local hash = vim.fn.sha256(cwd):sub(1, 8)
---       local session_file = vim.fn.stdpath 'data' .. '/sessions/' .. base .. '-' .. hash .. '.vim'
---
---       if vim.fn.filereadable(session_file) == 1 then
---         vim.cmd('source ' .. vim.fn.fnameescape(session_file))
---       end
---     end
---   end,
---   nested = true,
--- })
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -54,7 +54,7 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '→ ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '▸ ', trail = '·', nbsp = '␣', extends = '>', precedes = '<' }
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
