13fb451294bf973bdb8b44a464524150f80aebaa
Vladimir <18351400+en9inerd@users.noreply.github.com>
2025-02-07 10:56:02 -0500

    update zen mode plugin

--- .config/nvim/lua/plugins/zenmode.lua
+++ .config/nvim/lua/plugins/zenmode.lua
@@ -13,19 +13,5 @@ return {
       vim.wo.number = true
       vim.wo.rnu = true
     end)
-
-    vim.keymap.set('n', '<leader>zZ', function()
-      require('zen-mode').setup {
-        window = {
-          width = 100,
-          options = {},
-        },
-      }
-      require('zen-mode').toggle()
-      vim.wo.wrap = false
-      vim.wo.number = false
-      vim.wo.rnu = false
-      vim.opt.colorcolumn = '0'
-    end)
   end,
 }
