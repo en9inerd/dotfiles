13c39492009617e5180cc02cb6bcb4e0fc5883eb
Vladimir <18351400+en9inerd@users.noreply.github.com>
2025-05-31 16:22:22 -0400

    update nvim config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,10 +2,12 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    branch = 'master',
+    lazy = false,
+    main = 'nvim-treesitter.configs',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'c', 'go', 'html', 'javascript', 'typescript', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
