eb8f18bd194cdf5f0d54c3ea92805a6f9a152a51
Vladimir <18351400+en9inerd@users.noreply.github.com>
2026-02-03 14:34:21 -0500

    Updated dotfiles

--- home/.config/nvim/lua/plugins/blink.lua
+++ home/.config/nvim/lua/plugins/blink.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
   vim.pack.add {
     { src = 'https://github.com/rafamadriz/friendly-snippets' },
-    { src = 'https://github.com/L3MON4D3/LuaSnip' },
+    { src = 'https://github.com/L3MON4D3/LuaSnip', version = vim.version.range '^2' },
     { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range '^1' },
   }
 
